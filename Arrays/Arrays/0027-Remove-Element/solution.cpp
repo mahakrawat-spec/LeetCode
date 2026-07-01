ec3868e0910ad367be94157f57aef05a22a6f3c4
MAHAK RAWAT <25lbcs3081@culkomail.in>
2026-07-01 20:25:26 +0530

    Arrays/0027-Remove-Element/README.md

---
+++ Arrays/Arrays/0027-Remove-Element/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] != val){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    };
+    int main() {
+      Solution sol;
+      vector<int> nums = {3, 2, 2, 3}; 
+      int val = 3;
+      int k = sol.removeElement(nums, val);
+      for(int i = 0; i < k; i++) {
+          cout << nums[i] << " ";
+       }
+    return 0;
+    }
+};
